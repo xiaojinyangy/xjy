298710183e940af23b12139f8f75c5a3e68ef1f2
x1aoyang <851529665@qq.com>
2021-10-15 16:40:58 +0800

    1

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -21,7 +21,7 @@ class JobController extends Controller
     public function areaMouth(){
         $model = new AreaModel();
         $mouthModel = new ShopMouthModel();
-       $code =  $this->request->post('code')
+       $code =  $this->request->post('code');
        $areaData =  $model->query()->where(['status'=>1],2)->select(['id','area_name'])->get()->toArray();
        if($code == 1){
                 return rjson(200,'请求成功',$areaData);
